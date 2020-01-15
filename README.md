8bbc7a314d7acb00ac3f1dcfb7140752939b7c9e
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2020-01-15 17:44:49 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Desk Check
+
+Automatic generation of symbol tables throughout the execution of a Python code.
+
+## Purpose
+
+We developed this module for educational purposes. The output of this module is
+used to verify desk checks performed by students who are learning to code.
+
+## Usage
+
+To use this program, run:
+
+`python desk_check.py FILENAME.py`
+
+Where `FILENAME.py` is the name of the Python file that you wish be checked.
+
+## Disclaimer
+
+This module is in VERY early stage and I don't know how it behaves with larger
+pieces of software.
--- desk_check.py
+++ desk_check.py
@@ -1,98 +1,119 @@
 import sys
 from collections import namedtuple
-from pprint import pprint
 
 
-# This documentation may be useful: https://docs.python.org/2/library/inspect.html
+# This documentation may be useful: https://docs.python.org/3/library/inspect.html
 '''
 TODO
 Do not go inside functions that were not defined in the module
 '''
 
-src = open('example.py').read()
-src_lines = src.split('\n')
-
-stack = []
-history = []
-prev_line = None
-prev_lineno = None
-
-TraceData = namedtuple('TraceData', 'lineno,line,name_dicts,call_lineno')
-
-def build_name_dicts(frame, *, name_dicts=None):
-    f_name = frame.f_code.co_name
-    f_name_tuple = (f_name,)
-    if name_dicts:
-        name_dicts = {f_name_tuple + k: v for k, v in name_dicts.items()}
-    else:
-        name_dicts = {}
-    frame_dict = {}
-
-    variables = frame.f_code.co_varnames + frame.f_code.co_names
-    for v in variables:
-        # We are only interested in simple variables, not functions (callable)
-        if v in frame.f_locals and not callable(frame.f_locals[v]):
-            frame_dict[v] = frame.f_locals[v]
-    name_dicts[f_name_tuple] = frame_dict
-
-    # The first level frame is this module, that shouldn't be included
-    if frame.f_back and frame.f_back.f_back:
-        return build_name_dicts(frame.f_back, name_dicts=name_dicts)
-    return name_dicts
+TraceData = namedtuple('TraceData', 'line_i,line,name_dicts,call_line_i')
 
 
-def is_def(f_name, cur_line):
-    return cur_line and 'def ' in cur_line and f_name not in cur_line
+class DeskChecker:
+    def __init__(self):
+        self._reset()
+        self._check_function_name = self.check_str.__name__
 
+    def _reset(self):
+        self.stack = []
+        self.history = []
+        self.prev_line = None
+        self.prev_lineno = None
+        self.src = None
+        self.src_lines = []
 
-def trace(frame, event, arg):
-    global prev_line, prev_lineno
-    f_name = frame.f_code.co_name
-    lineno = frame.f_lineno
-    try:
-        cur_line = src_lines[lineno-1]
-    except IndexError:
-        cur_line = None
-    if event == 'line':
-        # print(cur_line)
-        name_dicts = build_name_dicts(frame)
-        # pprint(name_dicts)
-        # print()
-        if prev_line:
-            history.append(TraceData(prev_lineno, prev_line, name_dicts, None))
-        prev_line = cur_line
-        prev_lineno = lineno
-    elif event == 'call' and not is_def(f_name, cur_line):
-        if prev_line:
-            stack.append((prev_lineno, prev_line))
-        # print('CALL', cur_line, event, arg)
-        name_dicts = build_name_dicts(frame)
-        # pprint(name_dicts)
-        # print()
-        if prev_line:
-            history.append(TraceData(lineno, cur_line, name_dicts, prev_lineno))
-        prev_line = None
-        prev_lineno = None
-    elif event == 'return':
-        # print('RETURN', cur_line, event, arg)
-        name_dicts = build_name_dicts(frame)
-        # pprint(name_dicts)
-        # print()
-        if prev_line:
-            history.append(TraceData(prev_lineno, prev_line, name_dicts, None))
+    def _build_name_dicts(self, frame, *, name_dicts=None):
+        f_name = frame.f_code.co_name
+        if name_dicts:
+            name_dicts = {'{0},{1}'.format(f_name, k): v for k, v in name_dicts.items()}
+        else:
+            name_dicts = {}
+        frame_dict = {}
+
+        variables = frame.f_code.co_varnames + frame.f_code.co_names
+        for v in variables:
+            # We are only interested in simple variables, not functions (callable)
+            if v in frame.f_locals and not callable(frame.f_locals[v]):
+                frame_dict[v] = frame.f_locals[v]
+        name_dicts[f_name] = frame_dict
+
+        # We should stop when we reach the frame that contains this object (and consequently _check_function_name)
+        if frame.f_back and frame.f_back.f_code.co_name != self._check_function_name:
+            return self._build_name_dicts(frame.f_back, name_dicts=name_dicts)
+        return name_dicts
+
+    def _is_def(self, f_name, cur_line):
+        return cur_line and 'def ' in cur_line and f_name not in cur_line
+
+    def _trace(self, frame, event, arg):
+        f_name = frame.f_code.co_name
+        lineno = frame.f_lineno
         try:
-            prev_lineno, prev_line = stack.pop()
+            cur_line = self.src_lines[lineno-1]
         except IndexError:
-            prev_lineno, prev_line = None, None
+            cur_line = None
+        if event == 'line':
+            name_dicts = self._build_name_dicts(frame)
+            if self.prev_line:
+                self.history.append(TraceData(self.prev_lineno-1, self.prev_line, name_dicts, None))
+            self.prev_line = cur_line
+            self.prev_lineno = lineno
+        elif event == 'call' and not self._is_def(f_name, cur_line):
+            if self.prev_line:
+                self.stack.append((self.prev_lineno, self.prev_line))
+            name_dicts = self._build_name_dicts(frame)
+            if self.prev_line:
+                self.history.append(TraceData(lineno-1, cur_line, name_dicts, self.prev_lineno-1))
+            self.prev_line = None
+            self.prev_lineno = None
+        elif event == 'return':
+            name_dicts = self._build_name_dicts(frame)
+            if self.prev_line:
+                self.history.append(TraceData(self.prev_lineno-1, self.prev_line, name_dicts, None))
+            try:
+                self.prev_lineno, self.prev_line = self.stack.pop()
+            except IndexError:
+                self.prev_lineno, self.prev_line = None, None
 
-    return trace
+        return self._trace
+
+    def check_file(self, filename):
+        return self.check_str(open(filename).read())
+
+    def check_str(self, src_str):
+        self._reset()
+        self.src = src_str
+        self.src_lines = src_str.split('\n')
+
+        sys.settrace(self._trace)
+        exec(self.src)
+        sys.settrace(None)
+
+        return self.history
 
 
-sys.settrace(trace)
+if __name__ == "__main__":
+    from pprint import pprint
+    import json
 
-exec(src)
+    checker = DeskChecker()
+    if len(sys.argv) < 2:
+        history = checker.check_file('example.py')
 
-for h in history:
-    print(h.line)
-    pprint(h.name_dicts)
-    print()
+        for h in history:
+            print(h.line)
+            pprint(h.name_dicts)
+            print()
+    else:
+        history = checker.check_file(sys.argv[1])
+        try:
+            outfile = sys.argv[2]
+            if not outfile.endswith('.json'):
+                outfile += '.json'
+        except IndexError:
+            outfile = 'out.json'
+
+        with open(outfile, 'w') as f:
+            json.dump(history, f)
